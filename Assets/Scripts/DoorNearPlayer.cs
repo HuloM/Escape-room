990738b1ec0075ea6f2dbfcbb2eb5be4f2056f6f
cookieman768 <cookieman768@gmail.com>
2020-11-18 21:40:01 -0700

    Added outlines

---
+++ Assets/Scripts/DoorNearPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorNearPlayer : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    public GameObject player;
+    private static readonly int CharacterNearby = Animator.StringToHash("character_nearby");
+
+    private void Update()
+    {
+        var distance = Vector3.Distance(this.gameObject.transform.position, player.transform.position);
+        if (distance < 3)
+        {
+            _animator.SetBool(CharacterNearby, true);
+        }
+        else
+        {
+            _animator.SetBool(CharacterNearby, false);
+        }
+    }
+}
---
+++ Assets/Scripts/InteractRaycast.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractRaycast : MonoBehaviour
+{
+    private RaycastHit hit;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.Raycast( Camera.main.ScreenPointToRay( Input.mousePosition ), out hit, 1000f );
+        if (hit.collider.gameObject.CompareTag("Interactable"))
+        {
+            var outline = hit.collider.gameObject.AddComponent<Outline>();
+
+            outline.OutlineMode = Outline.Mode.OutlineAll;
+            outline.OutlineColor = Color.yellow;
+            outline.OutlineWidth = 5f;
+        }
+    }
+}
