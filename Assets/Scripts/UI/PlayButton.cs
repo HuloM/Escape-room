4952096199567e699c62380402551c8f0e039b0e
cookieman768 <cookieman768@gmail.com>
2020-11-20 21:01:19 -0700

    Added more assets

--- Assets/Scripts/UI/PlayButton.cs
+++ Assets/Scripts/UI/PlayButton.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 public class PlayButton : MonoBehaviour
 {
     public static string LevelToLoad;
-    
+
     [SerializeField] private string _levelName;
 
     private void Awake()
     {
+        AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.PlayOneShot((AudioClip) Resources.Load(path: "Assets/Audio/button_click.wav"));
         GetComponent<Button>().onClick.AddListener(() => LevelToLoad = _levelName);
     }
 }
