b66610ca2e6c37ff4a77f73699eafb340989b960
cookieman768 <cookieman768@gmail.com>
2020-11-21 20:58:46 -0700

    Added menu for NPC

---
+++ Assets/NPCScript.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class NPCScript : MonoBehaviour, IPuzzle
+{
+    
+    
+    [SerializeField] private Item _rewardItem;
+    [SerializeField] private GameObject UIBoard;
+
+    public TMP_Text DeselectionText { get; set; }
+    public bool CompleteFlag { get; set; }
+    public IItem RewardItem => _rewardItem;
+    public Player _player { get; set; }
+
+
+
+    private void Awake()
+    {
+        if(RewardItem == null)
+            NoRewardItem();
+        CompleteFlag = false;
+        _player = FindObjectOfType<Player>();
+    }
+
+    private void Update()
+    {
+        Interact.InteractWithObject(
+            UIBoard, _player, 
+            gameObject, DeselectionText);
+    }
+
+    private void GiveRewardItem() => _rewardItem.gameObject.SetActive(true);
+
+    public int NumberGeneration() => Random.Range(10000,99999);
+
+    public void NoRewardItem() => Debug.Log(
+        "Please add a reward item to be given on completion");
+    
+}
