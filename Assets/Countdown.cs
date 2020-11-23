f2d738adf3c0254aa8ab57c0a52ada3fa5a97c96
cookieman768 <cookieman768@gmail.com>
2020-11-22 21:39:48 -0700

    Added countdown timers

---
+++ Assets/Countdown.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+    private float currentTime;
+
+    private float startingTime = 60f;
+
+    private TMP_Text timerText;
+
+    public bool outOfTime = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+        timerText = gameObject.GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        timerText.text = "Time Remaining: " + (int) currentTime + "s";
+
+        if (currentTime <= 0)
+        {
+            outOfTime = true;
+        }
+    }
+}
