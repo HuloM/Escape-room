1a8fd842815ccb42cfb9b79ab72490d7b74a90da
cookieman768 <cookieman768@gmail.com>
2020-11-22 22:26:01 -0700

    Added win screen

---
+++ Assets/Scripts/EscapedScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EscapedScript : MonoBehaviour
+{
+    public GameObject winScreen;
+
+    private Slider score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GameObject.FindGameObjectWithTag("CompletePuzzle").GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (score.value >= 4)
+        {
+            winScreen.SetActive(true);
+        }
+    }
+}
